60f97b4bf047aaa396197cc6b35a6d896a847a73
Z.A <chryxus@gmail.com>
2020-10-03 20:57:11 -0700

    Create SysExhaustPlume
    SysExhaustPlume is a system that handles the exhaust effects of
    MachineRocket parts. A MachineRocket, upon instantiation, searches the
    scene hierarchy underneath itself for nodes whose name begins with
    `fx_plume_` and gives them a `ACompExhaustPlume`. This component stores
    the ID of the owning entity, and uses its throttle input to determines
    whether or not to print an annoying console message for informational
    purposes.

---
+++ src/adera/SysExhaustPlume.cpp
@@ -0,0 +1,116 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "SysExhaustPlume.h"
+#include "osp/Active/ActiveScene.h"
+#include "adera/Machines/Rocket.h"
+#include "osp/Resource/AssetImporter.h"
+#include "osp/Active/SysDebugRender.h"
+
+#include <Magnum/Trade/MeshData.h>
+#include <Magnum/MeshTools/Compile.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/Trade/ImageData.h>
+#include <Magnum/ImageView.h>
+#include <Magnum/GL/Sampler.h>
+#include <Magnum/GL/TextureFormat.h>
+
+using namespace osp::active;
+
+SysExhaustPlume::SysExhaustPlume(ActiveScene& rScene)
+    : m_scene(rScene), m_time(0.0f),
+    m_updatePlume(rScene.get_update_order(), "exhaust_plume", "mach_rocket", "",
+        [this](ActiveScene& rScene){this->update_plumes(rScene);})
+{}
+
+void SysExhaustPlume::initialize_plume(ActiveEnt node)
+{
+    using Magnum::GL::Mesh;
+    using Magnum::GL::Texture2D;
+    using Magnum::Trade::ImageData2D;
+    using namespace Magnum::Math::Literals;
+    using adera::shader::PlumeShader;
+    using ShaderInstance_t = PlumeShader::ACompPlumeShaderInstance;
+
+    auto const& plume = m_scene.reg_get<ACompExhaustPlume>(node);
+
+    if (m_scene.get_registry().try_get<ShaderInstance_t>(node))
+    {
+        return;  // plume already initialized
+    }
+
+    DependRes<PlumeEffectData> plumeEffect = plume.m_effect;
+
+    Package& pkg = m_scene.get_application().debug_find_package("lzdb");
+
+    // Get plume mesh
+    Package& glResources = m_scene.get_context_resources();
+    DependRes<Mesh> plumeMesh = glResources.get<Mesh>(plumeEffect->m_meshName);
+    if (plumeMesh.empty())
+    {
+        plumeMesh = AssetImporter::compile_mesh(plumeEffect->m_meshName, pkg, glResources);
+    }
+
+    // Get plume tex (TEMPORARY: just grab noise1024.png)
+    const std::string texName = "OSPData/adera/noise1024.png";
+    DependRes<Texture2D> n1024 = glResources.get<Texture2D>(texName);
+    if (n1024.empty())
+    {
+        n1024 = AssetImporter::compile_tex(texName, pkg, glResources);
+    }
+
+    // Emplace plume shader instance component from plume effect parameters
+    ShaderInstance_t shader(
+        glResources.get<PlumeShader>("plume_shader"),
+        n1024,
+        n1024,
+        *plumeEffect);
+    m_scene.reg_emplace<ShaderInstance_t>(node, std::move(shader));
+
+    m_scene.reg_emplace<CompDrawableDebug>(node, plumeMesh,
+        &adera::shader::PlumeShader::draw_plume);
+    m_scene.reg_emplace<CompVisibleDebug>(node, false);
+    m_scene.reg_emplace<CompTransparentDebug>(node, true);
+}
+
+void SysExhaustPlume::update_plumes(ActiveScene& rScene)
+{
+    using adera::active::machines::MachineRocket;
+    auto plumeView = m_scene.get_registry().view<ACompExhaustPlume>();
+
+    for (ActiveEnt plumeEnt : plumeView)
+    {
+        ACompExhaustPlume& plume = plumeView.get<ACompExhaustPlume>(plumeEnt);
+
+        auto& machine = m_scene.reg_get<MachineRocket>(plume.m_parentMachineRocket);
+        const auto& throttle = *machine.request_input(2)->connected_value();
+        const auto throttlePos = std::get<wiretype::Percent>(throttle).m_value;
+
+        if (throttlePos > 0.0f)
+        {
+            std::cout << "ON!\n";
+        }
+    }
+}
