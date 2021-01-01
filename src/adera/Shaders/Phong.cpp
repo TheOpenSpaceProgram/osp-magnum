b2d020b741463557d400e527f9e60c63a5dbd2bf
Z.A <chryxus@gmail.com>
2021-01-01 00:52:44 -0800

    PR Fixes
    I swear MSVC is way too permissive without enabling conformance mode

--- src/adera/Shaders/Phong.cpp
+++ src/adera/Shaders/Phong.cpp
@@ -13,7 +13,7 @@ void Phong::draw(ActiveEnt e,
     PhongShaderInstance& shaderInstance = scene.reg_get<PhongShaderInstance>(e);
     Phong& shader = *shaderInstance.m_shaderProgram;
 
-    Magnum::Matrix4& entRelative = camera.m_inverse * transform.m_transformWorld;
+    Magnum::Matrix4 entRelative = camera.m_inverse * transform.m_transformWorld;
 
     shader
         .bindDiffuseTexture(*shaderInstance.m_textures[0])
