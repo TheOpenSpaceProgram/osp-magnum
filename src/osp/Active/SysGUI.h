70fe7ec0f9840a827c0c2e41c28d910d26c4b020
Z.A <chryxus@gmail.com>
2021-02-23 23:21:45 -0800

    tmp stash old sysgui

---
+++ src/osp/Active/SysGUI.h
@@ -0,0 +1,55 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#if 0
+#include <functional>
+
+#include "osp/Active/activetypes.h"
+#include <Magnum/ImGuiIntegration/Context.hpp>
+
+namespace osp::active
+{
+
+struct CompGUIWindow
+{
+    std::function<void(ActiveScene&)> m_function;
+};
+
+class SysGUI : public IDynamicSystem
+{
+public:
+    static inline std::string smc_name = "GUI";
+
+    SysGUI(ActiveScene& rScene);
+    ~SysGUI();
+
+    static void update_GUI(ActiveScene& rScene);
+
+private:
+    UpdateOrderHandle_t m_updateGUI;
+    RenderOrderHandle_t m_drawGUI;
+};
+
+} // osp::active
+#endif
