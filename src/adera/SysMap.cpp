b19990982bf4b8eb661fed4e9839d375831185a3
Z.A <chryxus@gmail.com>
2021-04-04 00:13:28 -0700

    Add rough SysMap

---
+++ src/adera/SysMap.cpp
@@ -0,0 +1,117 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "SysMap.h"
+#include <Magnum/GL/Shader.h>
+#include <Magnum/GL/Version.h>
+#include <Corrade/Containers/Reference.h>
+
+using namespace Magnum;
+using namespace adera::active;
+using namespace osp::active;
+
+Vector3 SysMap::universe_to_render_space(osp::Vector3s v3s)
+{
+    constexpr int64_t units_per_m = 1024ll;
+    float x = static_cast<double>(v3s.x() / units_per_m) / 1e6;
+    float y = static_cast<double>(v3s.y() / units_per_m) / 1e6;
+    float z = static_cast<double>(v3s.z() / units_per_m) / 1e6;
+    return Vector3{x, y, z};
+}
+
+void SysMap::add_functions(ActiveScene& rScene)
+{
+    rScene.debug_update_add(rScene.get_update_order(),
+        "SystemMap", "", "", &update_map);
+}
+
+void SysMap::update_map(ActiveScene& rScene)
+{
+
+}
+
+void MapUpdateCompute::update_map(
+    size_t numPoints, GL::Buffer& pointBuffer,
+    size_t numPaths,  GL::Buffer& pathMetadata,
+    size_t numPathVerts, GL::Buffer& pathVertBuffer,
+    size_t numPathIndices, GL::Buffer& pathIndexBuffer)
+{
+    bind_point_locations(pointBuffer);
+    bind_path_vert_data(pathVertBuffer);
+    bind_path_index_data(pathIndexBuffer);
+    bind_path_metadata(pathMetadata);
+    set_uniform_counts(numPoints, numPaths, numPathVerts, numPathIndices);
+
+    Vector3ui nGroups{1, numPaths, 1};
+    dispatchCompute(nGroups);
+}
+
+void MapUpdateCompute::init()
+{
+    GL::Shader prog{GL::Version::GL430, GL::Shader::Type::Compute};
+    prog.addFile("OSPData/adera/Shaders/MapUpdate.comp");
+
+    CORRADE_INTERNAL_ASSERT_OUTPUT(prog.compile());
+    attachShader(prog);
+    CORRADE_INTERNAL_ASSERT_OUTPUT(link());
+}
+
+void MapUpdateCompute::set_uniform_counts(
+    size_t numPoints, size_t numPaths, size_t numPathVerts, size_t numPathIndices)
+{
+    setUniform(static_cast<Int>(EUniformPos::BlockCounts),
+        Vector4ui{numPoints, numPaths, numPathVerts, numPathIndices});
+}
+
+void MapUpdateCompute::bind_point_locations(GL::Buffer& points)
+{
+    points.bind(GL::Buffer::Target::ShaderStorage,
+        static_cast<Int>(EBufferBinding::PointVerts));
+}
+
+void MapUpdateCompute::bind_path_vert_data(GL::Buffer& pathVerts)
+{
+    pathVerts.bind(GL::Buffer::Target::ShaderStorage,
+        static_cast<Int>(EBufferBinding::PathData));
+}
+
+void MapUpdateCompute::bind_path_index_data(GL::Buffer& pathIndices)
+{
+    pathIndices.bind(GL::Buffer::Target::ShaderStorage,
+        static_cast<Int>(EBufferBinding::PathIndices));
+}
+
+void MapUpdateCompute::bind_path_metadata(Magnum::GL::Buffer& data)
+{
+    data.bind(GL::Buffer::Target::ShaderStorage,
+        static_cast<Int>(EBufferBinding::PathsInfo));
+}
+
+MapRenderData::MapRenderData(ActiveScene& scene, size_t maxPoints, size_t maxPathVertices)
+    : m_maxPoints(maxPoints), m_maxPathVerts(maxPathVertices)
+{
+    assert(maxPoints < std::numeric_limits<GLuint>::max());
+    assert(maxPathVertices < std::numeric_limits<GLuint>::max());
+}
---
+++ src/adera/SysMap.h
@@ -0,0 +1,154 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2020 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+#include <osp/Active/ActiveScene.h>
+#include <osp/types.h>
+#include <osp/Universe.h>
+
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/AbstractShaderProgram.h>
+
+#include <Magnum/Shaders/VertexColor.h>
+
+namespace adera::active
+{
+
+/*
+Device memory:
+- A buffer of vertices to represent all body positions
+- An indexed buffer of vertices to represent orbit paths
+
+Every frame, the buffer will be mmapped and new positions written. A compute
+shader can increment the index buffer and fade the orbits over time.
+*/
+class MapRenderData
+{
+    friend class MapUpdateCompute;
+public:
+    // "Primitive restart" index which signifies to break a line strip at that vertex
+    static constexpr GLuint PRIMITIVE_RESTART = std::numeric_limits<GLuint>::max();
+
+    MapRenderData(osp::active::ActiveScene& scene, size_t maxPoints, size_t maxPathVertices);
+    ~MapRenderData() = default;
+    MapRenderData(MapRenderData const& copy) = delete;
+    MapRenderData(MapRenderData&& move) = default;
+
+private:
+    GLuint m_maxPoints;
+    GLuint m_maxPathVerts;
+
+#pragma pack(push, 1)
+    struct ColorVert
+    {
+        Magnum::Vector4 m_pos;
+        Magnum::Color4 m_color;
+    };
+#pragma pack(pop)
+
+    // Path data
+    std::vector<GLuint> m_indexData;
+    Magnum::GL::Buffer m_indexBuffer;
+    std::vector<ColorVert> m_vertexData;
+    Magnum::GL::Buffer m_vertexBuffer;
+    Magnum::GL::Mesh m_mesh;
+
+    // Point object data
+    std::vector<ColorVert> m_points;
+    Magnum::GL::Buffer m_pointBuffer;
+    Magnum::GL::Mesh m_pointMesh;
+
+    struct PathMetadata
+    {
+        GLuint m_pointIndex;
+        GLuint m_startIdx;
+        GLuint m_endIdx;
+        GLuint m_nextIdx;
+    };
+
+    // Mapping from satellites to path metadata index
+    std::map<osp::universe::Satellite, size_t> m_pathMapping;
+    // Path metadata
+    std::vector<PathMetadata> m_pathMetadata;
+    Magnum::GL::Buffer m_pathMetadataBuffer;
+
+    // Mapping from satellites to point sprites
+    std::map<osp::universe::Satellite, GLuint> m_pointMapping;
+};
+
+class MapUpdateCompute : public Magnum::GL::AbstractShaderProgram
+{
+public:
+    MapUpdateCompute() { init(); }
+    ~MapUpdateCompute() = default;
+    MapUpdateCompute(MapUpdateCompute const& copy) = delete;
+    MapUpdateCompute(MapUpdateCompute&& move) = default;
+
+    void update_map(
+        size_t numPoints, Magnum::GL::Buffer& pointBuffer,
+        size_t numPaths, Magnum::GL::Buffer& pathMetadata,
+        size_t numPathVerts, Magnum::GL::Buffer& pathVertBuffer,
+        size_t numPathIndices, Magnum::GL::Buffer& pathIndexBuffer);
+private:
+    void init();
+
+    enum class EUniformPos : Magnum::Int
+    {
+        BlockCounts = 0,
+    };
+
+    enum class EBufferBinding : Magnum::Int
+    {
+        PointVerts = 0,
+        PathData = 1,
+        PathIndices = 2,
+        PathsInfo = 3
+    };
+
+    using Magnum::GL::AbstractShaderProgram::draw;
+    using Magnum::GL::AbstractShaderProgram::drawTransformFeedback;
+    using Magnum::GL::AbstractShaderProgram::dispatchCompute;
+
+    void set_uniform_counts(size_t numPoints, size_t numPaths,
+        size_t numPathVerts, size_t numPathIndices);
+    void bind_point_locations(Magnum::GL::Buffer& points);
+    void bind_path_vert_data(Magnum::GL::Buffer& pathVerts);
+    void bind_path_index_data(Magnum::GL::Buffer& pathIndices);
+    void bind_path_metadata(Magnum::GL::Buffer& data);
+};
+
+class SysMap
+{
+public:
+    static void add_functions(osp::active::ActiveScene& rScene);
+    static void update_map(osp::active::ActiveScene& rScene);
+
+private:
+    static Magnum::Vector3 universe_to_render_space(osp::Vector3s v3s);
+    static constexpr size_t m_orbitSamples = 512;
+};
+
+} // namespace adera::active
