75cbe9a264a6d907408839fcfbb64b16027a1c42
Neal_Nicdao <chrisnicdao0@gmail.com>
2022-01-27 20:17:31 -0800

    Progress on new Resources class

--- src/osp/Resource/resources.h
+++ src/osp/Resource/resources.h
@@ -24,32 +24,21 @@
  */
 #pragma once
 
+#include "resourcetypes.h"
+
 #include <longeron/id_management/registry.hpp>
+#include <longeron/id_management/refcount.hpp>
 
 #include <entt/core/family.hpp>
 #include <entt/core/any.hpp>
 
 #include <algorithm>
-#include <cstdint>
 #include <string_view>
 #include <unordered_map>
 #include <vector>
 
 namespace osp
 {
-
-enum class ResTypeId : uint32_t { };
-enum class ResId : uint32_t { };
-enum class PkgId : uint32_t { };
-
-template <typename T>
-struct resource_container
-{
-    using type = std::unordered_map<ResId, T>;
-};
-
-template <typename T>
-using resource_container_t = typename resource_container<T>::type;
 
 class Resources
 {
@@ -59,8 +48,13 @@ class Resources
     struct PerResType
     {
         lgrn::IdRegistry<ResId>         m_resIds;
+        lgrn::RefCount<int>             m_resRefs;
         std::vector<res_data_type_t>    m_resDataTypes;
         std::vector<entt::any>          m_resData;
+
+        // keys of m_nameToResId point to elements of m_resNames
+        std::vector<std::string>                        m_resNames;
+        std::unordered_map< std::string_view, ResId >   m_nameToResId;
     };
 
     struct PerPkgResType
@@ -71,99 +65,244 @@ class Resources
     struct PerPkg
     {
         std::vector<PerPkgResType> m_resTypeOwn;
-        // points to a key in m_pkgNames, might be a bit hacky
-        std::string_view m_name;
     };
 
 public:
 
-    void resize_types(std::size_t n)
+    /**
+     * @brief Resize to fit a certain number of resource types
+     *
+     * @param n [in] Number of types to support
+     */
+    inline void resize_types(std::size_t n)
     {
         m_perResType.resize(n);
     }
 
-    ResId create(ResTypeId typeId, PkgId pkgId, std::string_view name)
-    {
-        // Create ResId associated to specified ResTypeId
-        assert(m_perResType.size() > std::size_t(typeId));
-        PerResType &rPerResType = m_perResType[std::size_t(typeId)];
-        ResId const newResId = rPerResType.m_resIds.create();
+    /**
+     * @brief Create a new resource Id
+     *
+     * @param typeId    [in] Resource Type Id
+     * @param pkgId     [in] Package Id
+     * @param name      [in] String name identifier
+     *
+     * @return Newly created Resource Id
+     */
+    ResId create(ResTypeId typeId, PkgId pkgId, std::string_view name);
 
-        // Associate it with the package
-        assert(m_pkgData.size() > std::size_t(pkgId));
-        PerPkg &rPkg = m_pkgData[std::size_t(pkgId)];
-        assert(rPkg.m_resTypeOwn.size() > std::size_t(typeId));
-        PerPkgResType &rPkgType = rPkg.m_resTypeOwn[std::size_t(typeId)];
-        rPkgType.m_owned.resize(m_perResType.size());
-        rPkgType.m_owned.set(std::size_t(newResId));
+    ResId find(ResTypeId typeId, PkgId pkgId, std::string_view name) const noexcept;
 
-        return newResId;
-    }
+    void store(ResTypeId typeId, ResId resId, ResIdStorage_t &rStorage) noexcept;
 
+    void release(ResTypeId typeId, ResIdStorage_t &rStorage) noexcept;
+
+    /**
+     * @brief Register a datatype to a resource Id
+     *
+     * This allows a datatype to be associated with Resource Ids (ResId) of a
+     * certain type (ResTypeId).
+     *
+     * @param typeId [in] Resource type to associate data to
+     */
     template<typename T>
-    void data_register(ResTypeId typeId)
-    {
-        assert(std::size_t(typeId) < m_perResType.size());
-        PerResType &rPerResType = m_perResType[std::size_t(typeId)];
-        res_data_type_t const type = res_data_family_t::type<T>;
+    void data_register(ResTypeId typeId);
 
-        std::vector<res_data_type_t> &rTypes = rPerResType.m_resDataTypes;
-
-        // Check to make sure type isn't already registered
-        assert(std::find(rTypes.begin(), rTypes.end(), type) == rTypes.end());
-
-        rTypes.push_back(type);
-        rPerResType.m_resData.emplace_back(resource_container_t<T>());
-    }
-
+    /**
+     * @brief Assign data to a Resource Id
+     *
+     * Make sure T is registered beforehand with data_register
+     *
+     * @param typeId    [in] Resource Type Id
+     * @param resId     [in] Resource Id
+     * @param args      [in] Arguments to pass to constructor
+     *
+     * @return Reference to datatype created
+     */
     template<typename T, typename ... ARGS_T>
-    T& data_add(ResTypeId typeId, ResId resId, ARGS_T&& ... args)
-    {
-        using Container_t = resource_container_t<T>;
+    T& data_add(ResTypeId typeId, ResId resId, ARGS_T&& ... args);
 
-        res_data_type_t const type = res_data_family_t::type<T>;
-        assert(m_perResType.size() > std::size_t(typeId));
-        PerResType &rPerResType = m_perResType[std::size_t(typeId)];
+    /**
+     * @brief Get reference to data type associated with a resource
+     *
+     * Assert or undefined behaviour if data isn't found
+     *
+     * @param typeId    [in] Resource Type Id
+     * @param resId     [in] Resource Id
+     *
+     * @return Reference to found data
+     */
+    template<typename T>
+    T& data_get(ResTypeId typeId, ResId resId);
 
-        assert(rPerResType.m_resIds.exists(resId)); // Ensure resource ID exists
+    /**
+     * @copydoc Resources::data_get(typeId, resId)
+     */
+    template<typename T>
+    T const& data_get(ResTypeId typeId, ResId resId) const;
 
-        std::vector<res_data_type_t> &rTypes = rPerResType.m_resDataTypes;
-        auto typeFound = std::find(rTypes.begin(), rTypes.end(), type);
-        assert(typeFound != rTypes.end()); // Ensure type is registered
+    /**
+     * @brief Get pointer to data type to associated with a resource
+     *
+     * @param typeId    [in] Resource Type Id
+     * @param resId     [in] Resource Id
+     *
+     * @return Pointer to found data, nullptr if not found
+     */
+    template<typename T>
+    T* data_try_get(ResTypeId typeId, ResId resId);
 
-        size_t const typeIndex = std::distance(rTypes.begin(), typeFound);
-        Container_t &rContainer = entt::any_cast<Container_t&>(
-                                        rPerResType.m_resData[typeIndex]);
+    /**
+     * @copydoc Resources::data_get(typeId, resId)
+     */
+    template<typename T>
+    T const* data_try_get(ResTypeId typeId, ResId resId) const;
 
-        // Emplace without needing copy constructor
-        auto const & [newIt, success] = rContainer.try_emplace(
-                            resId, std::forward<ARGS_T>(args)...);
-
-        assert(success); // Emplace should always succeed
-
-        return newIt->second;
-    }
-
-    PkgId pkg_add(std::string_view name)
-    {
-        assert(m_pkgData.size() == m_pkgNames.size());
-
-        uint32_t const newPkgId = m_pkgData.size();
-
-        auto newIt = m_pkgNames.emplace(name, newPkgId);
-        m_pkgData.emplace_back(
-                PerPkg{ std::vector<PerPkgResType>(m_perResType.size()),
-                        newIt.first->first });
-
-        return PkgId(newPkgId);
-    }
+    /**
+     * @brief Create a new package
+     *
+     * Name, metadata, and other additional information on a package is best
+     * stored externally by associating the PkgId, not as a responsibility of
+     * this class.
+     *
+     * @return Package Id for newly created package
+     */
+    PkgId pkg_create();
 
 private:
-    std::vector<PerResType> m_perResType;
 
-    std::unordered_map<std::string, uint32_t> m_pkgNames;
+    PerResType const& get_type(ResTypeId typeId) const
+    {
+        assert(std::size_t(typeId) < m_perResType.size());
+        return m_perResType[std::size_t(typeId)];
+    }
+
+    PerResType& get_type(ResTypeId typeId)
+    {
+        assert(std::size_t(typeId) < m_perResType.size());
+        return m_perResType[std::size_t(typeId)];
+    }
+
+    template <typename T>
+    res_container_t<T>& get_container(PerResType &rPerResType, ResTypeId typeId);
+
+    template <typename T>
+    res_container_t<T> const& get_container(PerResType const &rPerResType, ResTypeId typeId) const;
+
+    std::vector<PerResType> m_perResType;
+    lgrn::IdRegistry<PkgId> m_pkgIds;
     std::vector<PerPkg> m_pkgData;
 };
 
+template<typename T>
+void Resources::data_register(ResTypeId typeId)
+{
+    res_data_type_t const type = res_data_family_t::type<T>;
+    PerResType &rPerResType = get_type(typeId);
+
+    std::vector<res_data_type_t> &rTypes = rPerResType.m_resDataTypes;
+
+    // Check to make sure type isn't already registered
+    assert(std::find(rTypes.begin(), rTypes.end(), type) == rTypes.end());
+
+    rTypes.push_back(type);
+    rPerResType.m_resData.emplace_back(res_container_t<T>{});
+}
+
+template<typename T, typename ... ARGS_T>
+T& Resources::data_add(ResTypeId typeId, ResId resId, ARGS_T&& ... args)
+{
+    PerResType &rPerResType = get_type(typeId);
+
+    // Ensure resource ID exists
+    assert(rPerResType.m_resIds.capacity() > std::size_t(typeId));
+    assert(rPerResType.m_resIds.exists(resId));
+
+    res_container_t<T> &rContainer = get_container<T>(rPerResType, typeId);
+
+    T& out = rContainer.emplace(resId, std::forward<ARGS_T>(args)...);
+
+    return out;
+}
+
+template<typename T>
+T& Resources::data_get(ResTypeId typeId, ResId resId)
+{
+    T* pData = data_try_get<T>(typeId, resId);
+    assert(pData != nullptr);
+    return *pData;
+}
+
+template<typename T>
+T const& Resources::data_get(ResTypeId typeId, ResId resId) const
+{
+    T const* pData = data_try_get<T>(typeId, resId);
+    assert(pData != nullptr);
+    return *pData;
+}
+
+template<typename T>
+T const* Resources::data_try_get(ResTypeId typeId, ResId resId) const
+{
+    static_assert(std::is_const_v<T>, "Non-const data requested from const Resources");
+    using NonConst_t = std::remove_const_t<T>;
+
+    PerResType const &rPerResType = get_type(typeId);
+
+    // Ensure resource ID exists
+    assert(rPerResType.m_resIds.capacity() > std::size_t(typeId));
+    assert(rPerResType.m_resIds.exists(resId));
+
+    res_container_t<NonConst_t> const &rContainer
+            = get_container<NonConst_t>(rPerResType, typeId);
+    return rContainer.get(resId);
+}
+
+template<typename T>
+T* Resources::data_try_get(ResTypeId typeId, ResId resId)
+{
+    PerResType &rPerResType = get_type(typeId);
+
+    // Ensure resource ID exists
+    assert(rPerResType.m_resIds.capacity() > std::size_t(typeId));
+    assert(rPerResType.m_resIds.exists(resId));
+
+    if constexpr (std::is_const_v<T>)
+    {
+        // Non-const requesting const
+        using NonConst_t = std::remove_const_t<T>;
+        res_container_t<NonConst_t> const &rContainer
+                = const_cast<const Resources*>(this)
+                    ->get_container<NonConst_t>(rPerResType, typeId);
+
+        return rContainer.get(resId);
+    }
+    else
+    {
+        res_container_t<T> &rContainer = get_container<T>(rPerResType, typeId);
+        return rContainer.get(resId);
+    }
+}
+
+template <typename T>
+res_container_t<T>& Resources::get_container(PerResType &rPerResType, ResTypeId typeId)
+{
+    res_data_type_t const type = res_data_family_t::type<T>;
+
+    std::vector<res_data_type_t> const &rTypes = rPerResType.m_resDataTypes;
+    auto typeFound = std::find(rTypes.cbegin(), rTypes.cend(), type);
+    assert(typeFound != rTypes.cend()); // Ensure type is registered
+
+    using Container_t = res_container_t<T>;
+
+    size_t const typeIndex = std::distance(rTypes.begin(), typeFound);
+
+    return entt::any_cast<Container_t&>(rPerResType.m_resData[typeIndex]);
+}
+
+template <typename T>
+res_container_t<T> const& Resources::get_container(PerResType const &rPerResType, ResTypeId typeId) const
+{
+    return const_cast<Resources*>(this)->get_container<T>(const_cast<PerResType&>(rPerResType), typeId);
+}
 
 } // namespace osp
---
+++ src/osp/Resource/resourcetypes.h
@@ -0,0 +1,111 @@
+/**
+ * Open Space Program
+ * Copyright © 2019-2021 Open Space Program Project
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
+#include <longeron/id_management/storage.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+namespace osp
+{
+
+enum class ResTypeId : uint32_t { };
+enum class ResId : uint32_t { };
+enum class PkgId : uint32_t { };
+
+template <typename T>
+class ResourceContainer
+{
+public:
+    // Required for std::is_copy_assignable to work properly inside of entt::any
+    ResourceContainer() = default;
+    ResourceContainer(ResourceContainer const& copy) = delete;
+    ResourceContainer(ResourceContainer&& move) = default;
+
+    template <typename ... ARGS_T>
+    T& emplace(ResId id, ARGS_T&& ... args)
+    {
+        m_vec.resize(std::max(m_vec.size(), std::size_t(id) + 1));
+        std::unique_ptr<T> &rPtr = m_vec[std::size_t(id)];
+        assert(!rPtr); // Ensure pointer is empty
+        rPtr = std::make_unique<T>(std::forward<ARGS_T>(args)...);
+        return *rPtr;
+    }
+
+    T const* get(ResId id) const
+    {
+        if(m_vec.size() <= std::size_t(id))
+        {
+            return nullptr;
+        }
+        return m_vec[std::size_t(id)].get();
+    }
+
+    T* get(ResId id)
+    {
+        return const_cast<T*>(const_cast<const ResourceContainer*>(this)->get(id));
+    }
+
+    void remove(ResId id)
+    {
+        assert(m_vec.size() > std::size_t(id));
+        m_vec[std::size_t(id)].reset();
+    }
+
+private:
+
+    std::vector< std::unique_ptr<T> > m_vec;
+};
+
+template <typename T>
+struct res_container;
+
+template <typename T>
+struct res_container
+{
+    using type = ResourceContainer<T>;
+};
+
+template <typename T>
+using res_container_t = typename res_container<T>::type;
+
+class Resources;
+
+using ResIdStorage_t = lgrn::IdStorage<ResId, Resources>;
+
+namespace restypes
+{
+
+constexpr inline ResTypeId const gc_image       = ResTypeId(0);
+constexpr inline ResTypeId const gc_texture     = ResTypeId(1);
+constexpr inline ResTypeId const gc_mesh        = ResTypeId(2);
+
+} // namespace restypes
+
+constexpr inline std::size_t const gc_resTypeCount = 3;
+
+} // namespace osp
